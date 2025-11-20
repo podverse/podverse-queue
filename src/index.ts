8a56eefff3880f81e5f7624763b7364e127fd11a
Mitch Downey <mitchldowney@gmail.com>
2025-11-20 01:35:04 -0600

    feat: add mqRSSAddRecentlyUpdatedFeedsFromPodcastIndex export to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './module-alias-config';
 
 export { mqRSSAdd } from './functions/mq/rss/add';
 export { mqRSSAddAll } from './functions/mq/rss/addAll';
+export { mqRSSAddRecentlyUpdatedFeedsFromPodcastIndex } from './functions/mq/rss/addRecentlyUpdatedFeedsFromPodcastIndex';
 export { mqRSSRunParser } from './functions/mq/rss/runParser';
   
 export { ActiveMQArtemisService, ActiveMQArtemisServiceParams } from './services/activeMQArtemis';
