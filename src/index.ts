41cbb9fc92ac8c2c8e4f25eb44da2442fb923a10
Mitch Downey <mitchldowney@gmail.com>
2025-12-12 01:49:07 -0600

    fix: remove unused 'getIsProcessing' export from mqRSSRunParser

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { mqRSSAdd } from './functions/mq/rss/add';
 export { mqRSSAddAll } from './functions/mq/rss/addAll';
 export { mqRSSAddRecentlyUpdatedFeedsFromPodcastIndex } from './functions/mq/rss/addRecentlyUpdatedFeedsFromPodcastIndex';
 export { mqRSSSetupDlqConsumers } from './functions/mq/rss/dlqHandling';
-export { mqRSSRunParser, getIsProcessing } from './functions/mq/rss/runParser';
+export { mqRSSRunParser } from './functions/mq/rss/runParser';
 export { mqRSSRunLiveItemListener } from './functions/mq/rss/runLiveItemListener';
   
 export { ActiveMQArtemisService, ActiveMQArtemisServiceParams } from './services/activeMQArtemis';
